1488c4206f8a3733e3662e9d161deb9bf62e62ab
Igor Bidiniuc <igorbidiniuc@gmail.com>
2025-06-15 21:55:51 +0100

    Initial Commit

---
+++ Garden/GardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenApp.swift
+//  Garden
+//
+//  Created by Igor Bidiniuc on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct GardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GardenTests/GardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  GardenTests.swift
+//  GardenTests
+//
+//  Created by Igor Bidiniuc on 15/06/2025.
+//
+
+import Testing
+@testable import Garden
+
+struct GardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
